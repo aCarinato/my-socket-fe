329e1889484b3e385a5c2ba2681c2d2738b09710
aCarinato <alessandro.carinato@gmail.com>
2022-10-31 11:41:25 +0100

    notifications

--- pages/messages.js
+++ pages/messages.js
@@ -12,6 +12,9 @@ function MessagesPage() {
 
   const [messages, setMessages] = useState([]);
 
+  const [notifications, setNotifications] = useState([]);
+  const [unreadNotifications, setUnreadNotifications] = useState(0);
+
   // This ref is for persisting the state of query string in url throughout re-renders. This ref is the value of query string inside url
   const openChatId = useRef();
   //   openChatId === router.query.message
@@ -65,11 +68,26 @@ function MessagesPage() {
       }
     };
 
+    const fetchNotifications = async () => {
+      try {
+        const userId = authState.userId;
+        const res = await axios.get(
+          `${process.env.NEXT_PUBLIC_API}/chats/notifications/${userId}`
+        );
+        // console.log(res);
+        setNotifications(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
     if (isLoggedIn) {
       fetchChats();
       fetchUsers();
+      fetchNotifications();
     }
   }, [isLoggedIn]);
+  //   console.log(notifications);
 
   //   SOCKET
   const socket = useRef();
@@ -214,6 +232,8 @@ function MessagesPage() {
             date: newMsg.date,
           };
 
+          //   THIS IS NOT setting chats in the backend!! setChats is only on the frontend.
+          //   The chats in the db are updated in app.js
           setChats((prev) => {
             const previousChat = Boolean(
               prev.find((chat) => chat.messagesWith === newMsg.sender)
@@ -228,12 +248,92 @@ function MessagesPage() {
               return [newChat, ...prev];
             }
           });
-        }
 
-        //     newMsgSound(senderName);
+          //   (maybe) here I need to set the notifications
+          const newNotification = {
+            type: 'newChatMsg',
+            from: newMsg.sender,
+            text: `You have a new message in the conversation with: ${username}`,
+            isRead: false,
+            date: Date.now(),
+          };
+          setNotifications((prev) => {
+            // find out if there are already notifications from that user
+            const previousNotifications = Boolean(
+              prev.find((notification) => notification.from === newMsg.sender)
+            );
+
+            if (previousNotifications) {
+              return [
+                newNotification,
+                ...prev.filter(
+                  (notification) => notification.from !== newMsg.sender
+                ),
+              ];
+            } else {
+              return [newNotification, ...prev];
+            }
+          });
+
+          //   and then emit an event to the backend to tell to add a new notification
+          //   console.log(newMsg.receiver);
+          socket.current.emit('sendNotification', {
+            senderId: newMsg.sender,
+            receiverId: newMsg.receiver,
+          });
+        }
       });
     }
   }, []);
+
+  const readNotification = (msgFrom) => {
+    console.log(msgFrom);
+    router.push(`/messages?message=${msgFrom}`, undefined, {
+      shallow: true,
+    });
+
+    // remove notifications from frontend
+    // setNotifications find the index of the current notification
+    const index = notifications.map((item) => item.from).indexOf(msgFrom);
+    // console.log(index);
+
+    if (index !== -1) {
+      // REMOVE NOTIFICATION (intial approach, then opted to read it)
+      // setNotifications((prev) => prev.splice(index, 1)); // DOES NOT WORK
+      //   setNotifications((prev) =>
+      //     prev.map((item) => item.from).filter((itm) => itm !== msgFrom)
+      //   );
+
+      // READ NOTIFICATION
+      setNotifications((prev) => {
+        const previousNotification = prev.find(
+          (notification) => notification.from === msgFrom
+        );
+        previousNotification.isRead = true;
+
+        return [...prev];
+      });
+    }
+
+    // emit event to remove notification from backend
+    const notificationTo = authState.userId;
+    socket.current.emit('readNotification', {
+      notificationTo,
+      msgFrom,
+    });
+  };
+
+  //   DISCONNECT
+  //   useEffect(() => {
+  //     if (socket.current) {
+  //       socket.current.on('disconnect', () => {
+  //         console.log('ciao');
+  //       });
+  //     }
+  //   }, []);
+
+  //   console.log(users);
+  //   console.log(connectedUsers);
 
   return (
     <div>
@@ -265,9 +365,31 @@ function MessagesPage() {
         )}
       </>
 
+      <>
+        {notifications && notifications.length > 0 && (
+          <>
+            <p>Notifications</p>
+            {notifications.map((notification) => (
+              <div
+                className={
+                  notification.isRead
+                    ? 'notification-card'
+                    : 'notification-card notification-unread'
+                }
+                key={notification._id}
+                onClick={() => readNotification(notification.from)}
+              >
+                {notification.text}
+              </div>
+            ))}
+            <br></br>
+          </>
+        )}
+      </>
+
       {chats.length > 0 ? (
         <>
-          <div>CIAO MONA</div>
+          <div>CIAO ALE IL GENIO</div>
           <div className="grid">
             <div>
               {chats.map((chat) => (
